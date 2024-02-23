15e1073836f7e49b4111356de4683e9988bca8f5
keni <kenikakadiya@gmail.com>
2024-02-23 19:41:02 +0530

    c

---
+++ p4.c
@@ -0,0 +1,38 @@
+
+//Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+//Input: num = 38
+//Output: 2
+//Explanation: The process is
+//38 --> 3 + 8 --> 11
+//11 --> 1 + 1 --> 2 
+//Since 2 has only one digit, return it.
+
+
+#include <stdio.h>
+
+int addDigits(int num) {
+    
+    if (num < 10) {
+        return num;
+    }
+
+ // Sum the digits
+    int sum = 0;
+    while (num > 0) {
+        sum = sum + num % 10;
+        num /= 10;
+    }
+
+    return addDigits(sum);
+}
+
+int main() {
+    int num = 9875; 
+
+    int result = addDigits(num);
+
+    printf("The digital root of %d is %d\n", num, result);
+
+    return 0;
+}
